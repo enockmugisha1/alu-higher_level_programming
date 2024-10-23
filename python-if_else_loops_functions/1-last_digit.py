a51563cd7ee492294d3aec7516c19714c85f3e62
Enock Mugisha <e.mugisha4@alustudent.com>
2024-10-23 08:01:43 -0700

    conditions

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -12,5 +12,7 @@ if last_digit > 5:
     print("and is greater than 5")
 elif last_digit == 0:
     print("and is 0")
+elif last_digit < 0:
+    print("is negative")
 else:
     print("and is less than 6 and not 0")
