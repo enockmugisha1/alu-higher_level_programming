d3e6c1d4f5324f31c1bfa4f78ea8d0308545c8de
Enock Mugisha <e.mugisha4@alustudent.com>
2024-10-24 04:32:42 -0700

    loops

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
+
+
 def islower(c):
     """Function checks for lowercase characters."""
     if ord(c) >= 97 and ord(c) <= 122:
         return True
     else:
-        return false
+        return False
