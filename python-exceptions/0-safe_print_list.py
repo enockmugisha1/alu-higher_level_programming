d0bf3864e86c1e5301ddccb51dd587eefa77cd8a
Enock Mugisha <e.mugisha4@alustudent.com>
2024-10-29 18:24:18 -0700

    exceptions and error handling

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    for x in range(my_list[x]):
+        try:
+            if x > my_list[x]:
+                return x
+        except SyntaxError:
+            print('you are wrong in writing codes')
