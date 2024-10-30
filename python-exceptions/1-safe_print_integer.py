d794c28d8ec6b823436f0f841634df0f8229d694
Enock Mugisha <e.mugisha4@alustudent.com>
2024-10-30 05:02:58 -0700

    exceptions and error handling

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -2,9 +2,8 @@
 
 
 def safe_print_integer(value):
-    """Prints an integer with "{:d}".format() and returns True if successful."""
-   try:
-        print("{:d}".format(value))  # Attempt to format and print the value as an integer
-        return True                   # Return True if the operation was successful
-    except (ValueError, TypeError):   # Catch ValueError and TypeError
-        return False      
+    try:
+     print("{:d}".format(value))
+        return True             
+  except (ValueError, TypeError):   
+    return False      
