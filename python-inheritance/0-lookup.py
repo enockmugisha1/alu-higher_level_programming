81a34139044594b83511171fa0e9d67fea2765f1
Enock Mugisha <e.mugisha4@alustudent.com>
2024-11-12 04:23:35 -0800

    inheritance

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+"""Lookup function"""
 
 
 def lookup(obj):
-    """this is my programs"""
+    """Returns the list of available attributes and methods of an object"""
     return dir(obj)
