31f878ce06f63e38db40fb8a515b9f13278aa0c7
Enock Mugisha <e.mugisha4@alustudent.com>
2024-11-12 17:23:30 -0800

    input and output

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-
 def read_file(filename=""):
-    with open("file.xt", "r", encoding = 'UTF8') as p:
-        print(p.read())
+    """Reads a text file (UTF-8) and prints its contents to stdout.
 
+    Args:
+        filename (str): The name of the file to read. Defaults to an empty string.
+    """
+    with open(filename, 'r', encoding='utf-8') as file:
+        print(file.read(), end="")
